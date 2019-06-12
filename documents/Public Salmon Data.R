e6a0058dd29be6b0bff0bfc5b3a30d7d1caad911
David Hernandez <davhernandez@ucdavis.edu>
2019-06-11 17:07:17 -0700

    Fix issue with calculating Norwegian exchange rate using apply.
    Imported part of the Faroe Island dataset
    
    Stuck on calculating the Faroe island exhcange rate with apply

--- documents/Public Salmon Data.R
+++ documents/Public Salmon Data.R
@@ -9,6 +9,44 @@ library(lubridate)
 dkk_exchange_rate <- read.csv("data/Salmon public data/DKKExchangeRate.csv")
 colnames(dkk_exchange_rate) <- c("Date", "Rate")
 
+#mutate to get a month and a year
+dkk_exchange_rate$Date <- as.Date(dkk_exchange_rate$Date, "%d-%b-%y")
+dkk_exchange_rate <- dkk_exchange_rate[-1,] %>%
+  mutate(Month = as.numeric(month(.$Date)),
+         Year = as.numeric(year(.$Date))) %>%
+  group_by(Month, Year) %>%
+  summarise(Rate = mean(Rate))
+
+#Faroe Island Monthly Exports to US ------------------------------------------------
+
+#Monthly price and weight of all salmon products exported to the US
+
+faroe_US_aggregate <- read_xlsx("data/Salmon public data/Faroe_exports_to_US.xlsx")
+
+colnames(faroe_US_aggregate) <- c("1","2","Date", "Price", "Weight")
+#select rows and columns to keep
+faroe_US_aggregate <- faroe_US_aggregate %>%
+  select(Date, Price, Weight) %>%
+  filter(!is.na(.$Date)) %>%
+  mutate(Price = as.numeric(Price),
+         Weight = as.numeric(Weight))
+
+#find and replace `-` with `0`
+faroe_US_aggregate[is.na(faroe_US_aggregate)] <- 0
+
+#split date into a year and month column
+faroe_US_aggregate$Year <- apply(faroe_US_aggregate, 1, function(x) as.numeric(substr(x["Date"], 1,4))) #Year
+faroe_US_aggregate$Month <- apply(faroe_US_aggregate, 1, function(x) as.numeric(substr(x["Date"], 6,7))) #Month
+
+
+#!!!!!!!!!!!the problem is that x["Price"] is changing the value to a character instead of just keeping it as a numeric. I could just wrap it in as.numeric, but that seems like a poor workaround !!!!!!!!!!!!!!
+
+#convert to USD
+dummy <- apply(faroe_US_aggregate,
+      1,
+      function(x) (x["Price"] / dkk_exchange_rate$Rate[which(dkk_exchange_rate$Month == x["Month"] & dkk_exchange_rate$Year == x["Year"])]))
+#match month and year to each other
+
 #NOK Exchange rate -----------------------------------------------------------
 
 nok_exchange_rate <- read.csv("data/Salmon public data/NOKExchangeRate.csv")
@@ -22,7 +60,10 @@ nok_exchange_rate <-  nok_exchange_rate %>%
   group_by(Date) %>%
   summarise(Rate = mean(Rate)) %>%
   mutate(Week = isoweek(Date), #get the week number
-         Year = as.numeric(format(as.Date(.$Date), "%Y"))) #convert `week` from factor to date, then convert that to a numeric
+         Year = isoyear(Date)) #get year according to ISO 8601 week calendar
+
+  #NOTE: `isoyear` returns the value as a decimal numer rather than an integer number (which is what as.numeric would do). This is why the `apply` function used below for the 
+
 
 #Norway weekly prices ----------------------------------------------------------
 
@@ -30,13 +71,10 @@ norway_prices <- read_xls("data/Salmon public data/NorwayPricesWeek.xls")
 colnames(norway_prices) <- norway_prices[8,] #use row 8 to grab column names
 norway_prices <- norway_prices[-(1:8),1:9] #keep only the columns of interest
 norway_prices <- as.data.frame(apply(norway_prices, 2, as.numeric))
+
 #convert to USD
-dummy <- left_join(norway_prices, nok_exchange_rate[,c("Year","Week","Rate")], by = c("Year", "Week"))
-
-
-#!!!!!!!!for some reason, it is cutting off three entries when it does the apply!!!!!!!!
-#!!!!!!!!find out what is going wrong!!!!!!!!!
-#trying this out by row
-as.data.frame(do.call(rbind,
-        apply(norway_prices, 1,  function(x)(x[3:9] / nok_exchange_rate$Rate[which(nok_exchange_rate$Year == x["Year"] & nok_exchange_rate$Week == x["Week"])]))
-))
+norway_prices[n,2:9] <- as.data.frame( #make it into a dataframe
+  t( #transpose the output b/c for some reason it is a matrix
+    apply(norway_prices, 1,  function(x)(x[3:9] / nok_exchange_rate$Rate[which(nok_exchange_rate$Year == x["Year"] & nok_exchange_rate$Week == x["Week"])])) #matches the exchange rate for the week and year of the row, then divides all of the values by the rate
+  )
+)
