a15ff7efff0ec5056b3bfc571cb65e2c32e6daaa
David Hernandez <davhernandez@ucdavis.edu>
2019-10-11 17:12:01 -0700

    There was a problem with the buffering because it was hard coded that the subsetted dataframe would be merged with buffered_monterey. Instead of fiddling too much, I wrote a clunkier version in the sandbox

--- documents/Pub plot 3.R
+++ documents/Pub plot 3.R
@@ -114,15 +114,15 @@ buffer_plots_function <- function(buffer_range, axes = FALSE, x_scale = c(0,3000
       dplyr::filter(Year <= 2014, Year >= 1985, Location == site, Effort < 40000) %>%
       group_by(Year) %>%
       summarise(CPUE = mean(Effort) * 0.0004535924) %>%
-      mutate(Location = "Monterey") %>%
-      merge(.,buffered_monterey, by = "Year") %>%
+      mutate(Location = site) %>% #!!!!!!!ummmm hard coded Monterey?!?!?!
+      merge(.,buffered_monterey, by = "Year") %>% #hard coded again!!!!!!!
       ggplot(aes(x = Otters, y = CPUE)) +
       geom_smooth(method = 'lm', color = "#0aa1ff", fill = "#9dd9ff", alpha = 1) +
       geom_point(color = "#0aa1ff", size = 2.5) +
       theme_classic() +
       theme(axis.title.x = element_blank(),
             axis.title.y = element_blank(),
-            axis.text.x = element_blank(),
+            #axis.text.x = element_blank(),
             axis.text.y = element_blank(),
             axis.ticks.length=unit(-0.1, "cm"),
             panel.border = element_rect(colour = "black", fill=NA, size=.5)) +
@@ -144,7 +144,7 @@ buffer_plots_function <- function(buffer_range, axes = FALSE, x_scale = c(0,3000
       group_by(Year) %>%
       summarise(CPUE = mean(Effort) * 0.0004535924) %>%
       mutate(Location = "Morro_Bay") %>%
-      merge(.,buffered_monterey, by = "Year") %>%
+      merge(.,buffered_morro, by = "Year") %>%
       ggplot(aes(x = Otters, y = CPUE)) +
       geom_smooth(method = 'lm', color = "#0aa1ff", fill = "#9dd9ff", alpha = 1) +
       geom_point(color = "#0aa1ff", size = 2.5) +
@@ -184,4 +184,146 @@ ggarrange(buffers_axis[[1]], buffers[[1]][[1]], buffers[[2]][[1]], buffers[[3]][
           buffers_axis[[3]], buffers[[1]][[3]], buffers[[2]][[3]], buffers[[3]][[3]], buffers[[4]][[3]],
           ncol = 5, nrow = 3,
           align = "hv") #aling both `h` and `v`
+
+
+
+#sandbox --------------------------------
+
+buffer_plots_function <- function(buffer_range, axes = FALSE, x_scale = c(0,3000)){
+  
+  #buffering function
+  port_buffering <- function(ports, ocean, distance){
+    port_geometry <- st_geometry(ports) #grabs the geometry of the port points
+    port_buffer <- st_buffer(port_geometry, dist = distance)#set a buffer around the port that of how far they might go
+    
+    within_buffer <- st_contains(port_buffer, ocean) #grab all the rows that are contained within the buffer
+    subset_list <- list() #empty list to fill
+    #loops over the list entries from 'within_buffer', subsetting the data based on row number.
+    #Each entry in the resulting list corresponds to the areas within a port's buffer
+    for(listentry in 1:length(within_buffer)){
+      subset_list[[listentry]] <- ocean[unlist(within_buffer[[listentry]]),]
+    }
+    return(subset_list)
+  }
+  
+  
+  #apply buffer to each st object
+  buffered_halfmoon <- do.call(rbind.data.frame,
+                               port_buffering(halfmoon_bay_st, SeaOtter_df,
+                                              distance = (buffer_range * 1000)))
+  #pull out only ATOS_IDs that are in the buffer zone
+  buffered_halfmoon <-otter_individuals[which(otter_individuals$ATOS_ID %in% unique(buffered_halfmoon$ATOS_ID)),] %>%
+    group_by(Year) %>%
+    summarise(Otters = sum(Count))
+  
+  buffered_monterey <- do.call(rbind.data.frame,
+                               port_buffering(monterey_bay_st, SeaOtter_df,
+                                              distance = buffer_range * 1000))
+  buffered_monterey <- otter_individuals[which(otter_individuals$ATOS_ID %in% unique(buffered_monterey$ATOS_ID)),] %>%
+    group_by(Year) %>%
+    summarise(Otters = sum(Count))
+  
+  buffered_morro <- do.call(rbind.data.frame,
+                            port_buffering(morro_bay_st, SeaOtter_df,
+                                           distance = (buffer_range * 1000)))
+  buffered_morro <-otter_individuals[which(otter_individuals$ATOS_ID %in% unique(buffered_morro$ATOS_ID)),] %>%
+    group_by(Year) %>%
+    summarise(Otters = sum(Count))
+  
+  #make the plots
+  output <- list()
+    #Monterey plot
+    output[[1]] <- readRDS("data/Crab Cleaned/Effort by Port") %>%
+      mutate(Year = as.numeric(format(.$Date, "%Y"))) %>%
+      dplyr::filter(Year <= 2014, Year >= 1985, Location == "Monterey", Effort < 40000) %>%
+      group_by(Year) %>%
+      summarise(CPUE = mean(Effort) * 0.0004535924) %>%
+      mutate(Location = "Monterey") %>%
+      merge(.,buffered_monterey, by = "Year") %>%
+      ggplot(aes(x = Otters, y = CPUE)) +
+      geom_smooth(method = 'lm', color = "#0aa1ff", fill = "#9dd9ff", alpha = 1) +
+      geom_point(color = "#0aa1ff", size = 2.5) +
+      theme_classic() +
+      theme(axis.title.x = element_blank(),
+            axis.title.y = element_blank(),
+            #axis.text.x = element_blank(),
+            axis.text.y = element_blank(),
+            axis.text.x = element_text(margin = margin(c(0.2,1), unit = "cm")),
+            axis.ticks.length=unit(-0.1, "cm"),
+            panel.border = element_rect(colour = "black", fill=NA, size=.5)) +
+      #coord_cartesian(xlim = x_scale, ylim = c(0, 0.5)) #use for stable axis
+      coord_cartesian(ylim = c(0, 0.5)) #use for floating x-axis
+
+  #Halfmoon Bay plot
+output[[2]] <- readRDS("data/Crab Cleaned/Effort by Port") %>%
+  mutate(Year = as.numeric(format(.$Date, "%Y"))) %>%
+  dplyr::filter(Year <= 2014, Year >= 1985, Location == "Halfmoon_Bay", Effort < 40000) %>%
+  group_by(Year) %>%
+  summarise(CPUE = mean(Effort) * 0.0004535924) %>%
+  mutate(Location = "Halfmoon_Bay") %>%
+  merge(.,buffered_halfmoon, by = "Year") %>%
+  ggplot(aes(x = Otters, y = CPUE)) +
+  geom_smooth(method = 'lm', color = "#0aa1ff", fill = "#9dd9ff", alpha = 1) +
+  geom_point(color = "#0aa1ff", size = 2.5) +
+  theme_classic() +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        #axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.text.x = element_text(margin = margin(c(0.2,1), unit = "cm")),
+        axis.ticks.length=unit(-0.1, "cm"),
+        panel.border = element_rect(colour = "black", fill=NA, size=.5)) +
+  #coord_cartesian(xlim = x_scale, ylim = c(0, 0.5)) #use for stable axis
+  coord_cartesian(ylim = c(0, 0.5)) #use for floating x-axis
+  
+  #make the plots
+  if(axes == FALSE){ #normal plots
+    output[[3]] <- readRDS("data/Crab Cleaned/Effort by Port") %>%
+      mutate(Year = as.numeric(format(.$Date, "%Y"))) %>%
+      dplyr::filter(Year <= 2014, Year >= 1985, Location == "Morro_Bay", Effort < 40000) %>%
+      group_by(Year) %>%
+      summarise(CPUE = mean(Effort) * 0.0004535924) %>%
+      mutate(Location = "Morro_Bay") %>%
+      merge(.,buffered_morro, by = "Year") %>%
+      ggplot(aes(x = Otters, y = CPUE)) +
+      geom_smooth(method = 'lm', color = "#0aa1ff", fill = "#9dd9ff", alpha = 1) +
+      geom_point(color = "#0aa1ff", size = 2.5) +
+      theme_classic() +
+      theme(axis.title.x = element_blank(),
+            axis.title.y = element_blank(),
+            #axis.text.x = element_blank(),
+            axis.text.y = element_blank(),
+            axis.ticks.length=unit(-0.1, "cm"),
+            panel.border = element_rect(colour = "black", fill=NA, size=.5)) +
+      #coord_cartesian(xlim = x_scale, ylim = c(0, 0.5)) #use for stable axis
+      coord_cartesian(ylim = c(0, 0.5)) #use for floating x-axis
+  } else if(axes == TRUE){
+    #for the single plot that needs axis labels
+    #plot with axis labels
+    output[[3]] <-     readRDS("data/Crab Cleaned/Effort by Port") %>%
+      mutate(Year = as.numeric(format(.$Date, "%Y"))) %>%
+      dplyr::filter(Year <= 2014, Year >= 1985, Location == "Morro_Bay", Effort < 40000) %>%
+      group_by(Year) %>%
+      summarise(CPUE = mean(Effort) * 0.0004535924) %>%
+      mutate(Location = "Morro_Bay") %>%
+      merge(.,buffered_morro, by = "Year") %>%
+      ggplot(aes(x = Otters, y = CPUE)) +
+      geom_smooth(method = 'lm', color = "#0aa1ff", fill = "#9dd9ff", alpha = 1) +
+      geom_point(color = "#0aa1ff", size = 2.5) +
+      ylab("metric tons per offload receipt") +
+      xlab("otter population size") +
+      theme_classic() +
+      theme(#axis.title.x = element_blank(),
+        #axis.title.y = element_blank(),
+        axis.text.x = element_text(margin = margin(c(0.2,1), unit = "cm")),
+        axis.text.y = element_text(margin = margin(c(1, 0.2), unit = "cm")),
+        axis.ticks.length=unit(-0.1, "cm"),
+        panel.border = element_rect(colour = "black", fill=NA, size=.5)) +
+      #coord_cartesian(xlim = x_scale, ylim = c(0, 0.5)) #use for stable axis
+      coord_cartesian(ylim = c(0, 0.5)) #use for floating x-axis
+    
+    
+  }
+  return(output)
+}
 
